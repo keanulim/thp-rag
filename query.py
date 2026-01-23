1e38bcbc627cecc338b0a6fc052c177069f9cb2d
keanulim <keanu7k@gmail.com>
2026-01-23 11:28:53 -0800

    Change question in retrieval chain invocation
    Updated the question asked to the retrieval chain for better relevance.

--- query.py
+++ query.py
@@ -7,22 +7,18 @@ import os
 
 os.environ["GOOGLE_API_KEY"] = "API_KEY"
 os.environ["PINECONE_API_KEY"] = "API_KEY"
-# 1. Initialize the Brain & Ears
+
 embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
 llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview", temperature=0)
 
-# 2. Connect to the Index you just built
 vectorstore = Pinecone(index_name="vetted-vertical", embedding=embeddings)
 
-# 3. Create the Retrieval Chain
-# This pulls the top 3 most relevant chunks from Pinecone
 retriever = vectorstore.as_retriever(search_kwargs={"k": 10})
 retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
 
 combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
 rag_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
-# 4. Ask a Vetted Question
-response = rag_chain.invoke({"input": "how many sets should i do for each exercise?"})
+response = rag_chain.invoke({"input": "whats the best exercise to increase my vertical jump?"})
 
 print(response["answer"])
